8fe0e8ad1a52615cacb1b8568ca73679c9a9910e
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-10-27 16:29:14 -0500

    cambio solo para despliegue

--- src/components/DatingCards.js
+++ src/components/DatingCards.js
@@ -4,8 +4,8 @@ import './DatingCards.css'
 
 const DatingCards = () =>{
     
-    //declaramos el estado inical del  componente, este caso iniciamos un arreglo de people
-    const [people, setPeople] = useState([
+    //, setPeople   declaramos el estado inical del  componente, este caso iniciamos un arreglo de people
+    const [people] = useState([
         { 
             name: "Random Guy", imgUrl: "https://images.unsplash.com/photo-1520409364224-63400afe26e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80" 
         },
