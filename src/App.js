0096b76376cfc05d42ff996bc0cfc06c058d71bd
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-10-27 16:18:28 -0500

    primera vista de app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 
 
 //import './App.css';
+//para deploy en firebase hosting
+//https://medium.com/swlh/how-to-deploy-a-react-app-with-firebase-hosting-98063c5bf425
+//https://newbedev.com/firebase-hosting-setup-complete-code-example
+//mejor 
+//https://cming0721.medium.com/build-and-deploy-react-app-on-firebase-in-2021-80b37cc52707
+//https://cming0721.medium.com/build-and-deploy-react-app-on-firebase-in-2021-80b37cc52707
 import Header from './components/Header';
 import DatingCards from './components/DatingCards';
 import SwipeButtons from './components/SwipeButtons';
