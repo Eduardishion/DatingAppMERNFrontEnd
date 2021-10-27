b215c556b639095e8352f77ed072707b12918054
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-10-27 17:56:12 -0500

    se agregaron endpoints para servidores local y remoto

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: "http://localhost:8001"
+    //local
+    //baseURL: "http://localhost:8001"
+    //remoto
+    baseURL: "https://app-mern-eduardihion.herokuapp.com"
 })
 export default instance;
